50b7073f21e1e069b0271af13e8a733691903aa3
fauzanr <fauzanr17@gmail.com>
2024-08-08 18:49:57 +0700

    initial & html content

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,186 @@
-import Image from "next/image";
+import Link from "next/link";
+import cx from "classnames";
+
+const Navbar = () => {
+  const navs = ["Homepage", "Customize Your Trip", "Destination", "Article"];
+  return (
+    <div className="flex">
+      <div>LOGO</div>
+      <div>
+        {navs.map((nav) => (
+          <Link href="#">{nav}</Link>
+        ))}
+      </div>
+      <button>Need Assistance?</button>
+    </div>
+  );
+};
 
 export default function Home() {
+  const year = new Date().getFullYear();
+  const experiences = [
+    {
+      Icon: <>Icon</>,
+      title: "PERSONALIZED ITINERARIES",
+      text: "Our premium travel services offer tailor-made itineraries crafted to suit you unique preferences and desires.",
+    },
+    {
+      Icon: <>Icon</>,
+      title: "EXCLUSIVE EXPERIENCES",
+      text: "From private charters to behind-the-scenes tours, we offer access to unique opportunities that area designed to eleveate your trip to the next level.",
+    },
+    {
+      Icon: <>Icon</>,
+      title: "BEST FACILITIES",
+      text: "Experience the epitome of with our premium facility, designed to provicde an unparalleled level of comfort and indulgence.",
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <>
+      <Navbar />
+      {/*  */}
+      <section>
+        <h1>Premium Travel</h1>
+        <p>Beyond Expectation</p>
+        <p>
+          Experience the finest that Indonesia has to offer with our curated
+          selection of premium trips, ensuring comfort every step of the way
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <button>Take Me There</button>
+      </section>
+      {/*  */}
+      <section>
+        <h1>Beyond Premium</h1>
+        <p>ELEVATE YOUR EXPERIENCE</p>
+        <div className="flex">
+          {experiences.map((exp) => (
+            <div key={exp.title} className="flex flex-1 flex-col items-center">
+              <div className="bg-orange-200 w-32 aspect-square">{exp.Icon}</div>
+              <h2>{exp.title}</h2>
+              <p>{exp.text}</p>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {/*  */}
+        <div className="flex">
+          <div className="flex-1">SVG</div>
+          <div className="flex-1">
+            <h1>Discover Tailored Experiences</h1>
+            <p>
+              Create your own journey, personalized to suit your preferences and
+              interests, ensuring a once-in-a-lifetime adventure awaits.
+            </p>
+            <button>Customize Your Trip</button>
+          </div>
+        </div>
+        {/*  */}
+        <hr />
+        {/*  */}
+        <div className="flex">
+          <h1>Destinations</h1>
+          <button>EXPLORE MORE</button>
+        </div>
+        {Array.from({ length: 3 }).map((_, idx) => (
+          <div
+            key={idx}
+            className={cx([
+              "flex gap-6",
+              { "flex-row-reverse": idx % 2 === 1 },
+            ])}
+          >
+            <div className="flex-1">
+              <span>7 DAYS 6 NIGHTS</span>
+              <h1>Paradise Gateway: Labuan Bajo</h1>
+              <span>Organized by Pandooin</span>
+              <p>
+                Labuan Bajo is a tropical paradise nestled in the westernmost
+                part of Flores Island, Indonesia. With its stunning landscapes,
+                crystal-clear waters, and vibrant marine life, it's a gateway to
+                explore the mesmerizing Komodo National Park.
+              </p>
+              <div className="flex justify-between items-end">
+                <div className="flex flex-col">
+                  <span>Start from</span>
+                  <span>IDR 9,999,999</span>
+                  <span>IDR 7,500,000</span>
+                </div>
+                <button>See Details</button>
+              </div>
+            </div>
+            <div className="flex-1 bg-blue-200">*Photo*</div>
+          </div>
+        ))}
+        <div className="flex gap-6">
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <div key={idx} className="flex flex-col flex-1">
+              <div className="bg-blue-200 aspect-square">*image*</div>
+              <span>7 DAYS 6 NIGHTS</span>
+              <h2>Paradise Gateway: Labuan Bajo</h2>
+              <span>Organized by Pandooin</span>
+              <div>
+                <span>Start from</span>
+                <span>IDR 5,200,000</span>
+              </div>
+              <button>See Details</button>
+            </div>
+          ))}
+        </div>
+        <button>EXPLORE MORE</button>
+      </section>
+      {/*  */}
+      <section>
+        <h1>Luxury Footages</h1>
+        <div className="flex gap-6">
+          <div className="flex-1 aspect-square bg-blue-200"></div>
+          <div className="flex-1 aspect-square bg-blue-200"></div>
+          <div className="flex-1 aspect-square bg-blue-200"></div>
+        </div>
+        <hr className="my-3" />
+        <div className="flex gap-6">
+          <div className="flex-1 aspect-square bg-blue-200"></div>
+          <div className="flex-1 aspect-square bg-blue-200"></div>
+          <div className="flex-1 aspect-square bg-blue-200"></div>
+        </div>
+      </section>
+      {/*  */}
+      <section>
+        <div className="flex justify-between">
+          <div>*logo*</div>
+          <div>
+            <p>Want to see other destinations? Check us out at our website</p>
+            <Link href="#">Panddoin.com</Link>
+          </div>
+        </div>
+        <h1>Articles</h1>
+        <p>Our curated writings, offering something for every reader.</p>
+        <div className="grid grid-cols-4 gap-6">
+          {Array.from({ length: 5 }).map((_, idx) => (
+            <div
+              key={idx}
+              className={cx([
+                "bg-blue-200 aspect-[4/3]",
+                {
+                  "grid-cols-subgrid row-span-2 col-span-2 h-full": idx === 0,
+                },
+              ])}
+            >
+              *image*
+            </div>
+          ))}
+        </div>
+      </section>
+      {/*  */}
+      <section>
+        <div className="flex justify-between">
+          <span>&copy; {year} Zamrood by PT Teknologi Pandu Wisata</span>
+          <div className="flex gap-4">
+            <span>Icon</span>
+            <span>Icon</span>
+            <span>Icon</span>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
