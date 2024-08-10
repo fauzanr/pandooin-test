0796141e815bb80f00076738243c5ca18f19f897
fauzanr <fauzanr17@gmail.com>
2024-08-10 23:29:44 +0700

    auto preview footages

--- app/section/footages/index.tsx
+++ app/section/footages/index.tsx
@@ -3,11 +3,11 @@
 import Container from "@/components/container";
 import { SeparatorWh } from "@/components/svg";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 const imageLength = 6;
 
-const AutoPreview = () => {
+const SectionFootages = () => {
   const [currentImageIdx, setCurrentImageIdx] = useState(0);
 
   const nextIdx = () => {
@@ -24,55 +24,43 @@ const AutoPreview = () => {
     };
   }, []);
 
-  return (
-    <div className="flex-1 aspect-[358/256] min-w-full sm:min-w-0 relative">
-      <Image
-        src={`https://picsum.photos/seed/${currentImageIdx}/580/400`}
-        fill
-        className="object-cover object-center"
-        alt="image"
-      />
-    </div>
+  const Images = useMemo(
+    () =>
+      Array.from({ length: imageLength }).map((_, idx) => (
+        <div className="flex-1 aspect-square min-w-0 relative" key={idx}>
+          <Image
+            src={`https://picsum.photos/seed/${idx + 1}/350/350`}
+            fill
+            className="object-cover object-center"
+            alt="image"
+          />
+        </div>
+      )),
+    []
   );
-};
 
-const SectionFootages = () => {
+  const AutoPreviewImage = Images[currentImageIdx];
+
   return (
     <section className="bg-tan py-8 px-4">
       <Container>
         <h1 className="font-the-signature text-[54px] text-dark">
           Luxury Footages
         </h1>
-        <div className="sm:hidden">
-          <AutoPreview />
-        </div>
+        <div className="sm:hidden">{AutoPreviewImage}</div>
         <div className="hidden sm:block">
           <div className="flex gap-6 overflow-x-auto">
-            {Array.from({ length: 3 }).map((_, idx) => (
-              <div className="flex-1 aspect-square min-w-0 relative" key={idx}>
-                <Image
-                  src={`https://picsum.photos/seed/${idx + 1}/350/350`}
-                  fill
-                  className="object-cover object-center"
-                  alt="image"
-                />
-              </div>
-            ))}
+            {Array.from({ length: imageLength / 2 }).map(
+              (_, idx) => Images[idx]
+            )}
           </div>
           <div className="flex justify-center">
             <SeparatorWh />
           </div>
           <div className="flex gap-6">
-            {Array.from({ length: 3 }).map((_, idx) => (
-              <div className="flex-1 aspect-square min-w-0 relative" key={idx}>
-                <Image
-                  src={`https://picsum.photos/seed/${idx + 4}/350/350`}
-                  fill
-                  className="object-cover object-center"
-                  alt="image"
-                />
-              </div>
-            ))}
+            {Array.from({ length: imageLength / 2 }).map(
+              (_, idx) => Images[idx + imageLength / 2]
+            )}
           </div>
         </div>
       </Container>
